f658e9675c535e734fd2ab3f6c7f2035a51fd498
andydev <akanshul97@gmail.com>
2018-02-11 19:17:14 -0500

    Added define tags for IMAGE CHANNELS

--- CudaFractal/CudaFractal.cu
+++ CudaFractal/CudaFractal.cu
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// PNG Image format
+#define IMAGE_NUM_CHANNELS 4
+#define IMAGE_RED_CHANNEL 0
+#define IMAGE_GREEN_CHANNEL 1
+#define IMAGE_BLUE_CHANNEL 2
+#define IMAGE_ALPHA_CHANNEL 3
+
 /**
  * Runs an operation on the given pixel space
  *
@@ -12,24 +19,39 @@
  * @param img   the image buffer
  */
 __global__
-void pixels(unsigned img_w, unsigned img_h, unsigned img_c, unsigned char* img) {
+void pixels(unsigned img_w, unsigned img_h, unsigned char* img) {
 	// Get x and y of image
-	// Don't run pixels beyond size on cu_img
+	// Don't run pixels beyond size on img
 	int y = blockIdx.x * blockDim.x + threadIdx.x;
 	int x = blockIdx.y * blockDim.y + threadIdx.y;
 	if (x >= img_w || y >= img_h) return;
+
+	// Get real and imaginary components of constant
+	// float creal = -0.4;
+	// float cimag = 0.6;
 	
-	// Get colors
-	unsigned char r = (x * 256) / img_w;
-	unsigned char g = (x * 256) / img_w;
-	unsigned char b = (y * 256) / img_h;
-	unsigned char a = 0xff;
+	// Get real and imaginary components of starting value
+	// float zreal = 2.0 * ((float)x) / img_w;
+	// float zimag = 2.0 * ((float)x) / img_h;
+
+	// printf("Pixel %i,%i -> %f + %fj\n", x, y, zreal, zimag);
+
+	// Juliaset Algorithm
+	int iters = 60;
+	//  for (iters = 0; iters < 256; iters++) {
+		// Break if we exceed the modulus
+	// 	if (zreal*zreal + zimag*zimag >= 4) break;
+
+		// Run iteration of z: z = z^2 + c
+	// 	zreal = zreal*zreal - zimag*zimag + creal;
+	// 	zimag = 2*zreal*zimag + cimag;
+	// }
 
 	// Append colors to image buffer
-	img[(y*img_w + x)*img_c + 0] = r;
-	img[(y*img_w + x)*img_c + 1] = g;
-	img[(y*img_w + x)*img_c + 2] = b;
-	img[(y*img_w + x)*img_c + 3] = a;
+	img[(y*img_w + x)*IMAGE_NUM_CHANNELS + IMAGE_RED_CHANNEL]   = iters; // Red
+	img[(y*img_w + x)*IMAGE_NUM_CHANNELS + IMAGE_GREEN_CHANNEL] = iters; // Green
+	img[(y*img_w + x)*IMAGE_NUM_CHANNELS + IMAGE_BLUE_CHANNEL]  = iters; // Blue
+	img[(y*img_w + x)*IMAGE_NUM_CHANNELS + IMAGE_ALPHA_CHANNEL] = 0xff;  // Alpha
 }
 
 /**
@@ -44,8 +66,7 @@ int main(int argc, const char* argv[]) {
 	// Image meta
 	unsigned img_w = 1920;
 	unsigned img_h = 1080;
-	unsigned img_c = 4;
-	unsigned img_l = img_h*img_w*img_c;
+	unsigned img_l = img_h*img_w*IMAGE_NUM_CHANNELS;
 	unsigned char* img;
 
 	// Process meta
@@ -59,7 +80,7 @@ int main(int argc, const char* argv[]) {
 
 	// Call Cuda Routine
 	printf("Running CUDA routine...");
-	pixels<<<gridSize, blockSize>>>(img_w, img_h, img_c, img);
+	pixels<<<gridSize, blockSize>>>(img_w, img_h, img);
 	cudaDeviceSynchronize();
 	printf("Done!\n");
 
